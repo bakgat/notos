f0280784c8906586460d8ee390c21b7b7b79efc2
bakgat <karl.vaniseghem@klimtoren.be>
2015-09-09 09:36:35 +0200

    not using the SimpleAnnotationReader

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,7 +4,7 @@ use Doctrine\ORM\Tools\Setup;
 require_once __DIR__ . "/../vendor/autoload.php";
 // Create a simple "default" Doctrine ORM configuration for XML Mapping
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/../src/Domain/Model"), $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/../src/Domain/Model"), $isDevMode, null, null, false);
 // or if you prefer yaml or annotations
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
