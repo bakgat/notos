2ae90cd0a38e082adf893101d1da9f6002b15148
bakgat <karl.vaniseghem@klimtoren.be>
2015-09-21 15:35:15 +0200

    vendor paths for doctrine cli

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,13 +1,13 @@
 <?php
 use Doctrine\ORM\Tools\Setup;
 
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once app_path(). "/vendor/autoload.php";
 
 // Bootstrap the JMS custom annotations for Object to Json mapping
 // Bootstrap the JMS custom annotations for Object to Json mapping
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
     'JMS\Serializer\Annotation',
-    __DIR__ . '/../vendor/jms/serializer/src'
+    app_path() . '/vendor/jms/serializer/src'
 );
 $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/../src/Domain/Model"), $isDevMode, null, null, false);
