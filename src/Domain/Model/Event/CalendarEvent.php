55c19e0dbe60bebaec2705602484d4f60b4b1de4
bakgat <karl.vaniseghem@klimtoren.be>
2015-09-30 14:02:02 +0200

    first commit

---
+++ src/Domain/Model/Event/CalendarEvent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 30/09/15
+ * Time: 13:53
+ */
+
+namespace Bakgat\Notos\Domain\Model\Event;
+
+
+class CalendarEvent
+{
+
+}
---
+++ src/Domain/Model/Event/Event.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 30/09/15
+ * Time: 13:48
+ */
+
+namespace Bakgat\Notos\Domain\Model\Events;
+
+
+class Event
+{
+
+}
