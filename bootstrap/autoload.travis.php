537e99a46b74e21f12321b2df7679b04be6c126a
bakgat <karl.vaniseghem@klimtoren.be>
2015-11-10 15:18:09 +0100

    support for travis db testing

--- bootstrap/autoload.travis.php
+++ bootstrap/autoload.travis.php
@@ -16,12 +16,9 @@ $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/src/Dom
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 // database configuration parameters
 $conn = [
-    'driver' => 'mysqli',
-    'host' => '127.0.0.1',
-    'dbname' =>'notos_test',
-    'user' => 'travis',
-    'password' => '',
-    'prefix' => ''
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__.'/notostest',
+
 ];
 
 
@@ -30,55 +27,3 @@ $entityManager = \Doctrine\ORM\EntityManager::create($conn, $config);
 $connection = $entityManager->getConnection();
 
 $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
-/*$metadatas = $entityManager->getMetadataFactory()->getAllMetadata();
-
-$schemaTool->dropSchema($metadatas);
-$schemaTool->createSchema($metadatas);*/
-$metadataNamespace = 'Bakgat\\Notos\\Domain\\Model\\';
-$resetTables = [
-    'Identity\Party',
-    'Identity\User',
-    'Identity\Group',
-    'Identity\Organization',
-    'Resource\Resource',
-    'Resource\Asset',
-    'Resource\Book',
-    'Resource\Image',
-    'Location\Location',
-    'Location\Blog',
-    'Location\Website',
-    'Relations\Relation',
-    'Relations\PartyRelation',
-    'ACL\Role',
-    'ACL\UserRole',
-    'ACL\Permission',
-    'Curricula\Course',
-    'Curricula\Curriculum',
-    'Curricula\Objective',
-    'Curricula\ObjectiveControlLevel',
-    'Curricula\Structure',
-    'Kind'
-];
-
-/*
- * CLEAN TABLES BEFORE START
- */
-$connection = $entityManager->getConnection();
-$dbPlatform = $connection->getDatabasePlatform();
-
-foreach ($resetTables as $name) {
-    $className = $metadataNamespace . $name;
-
-    $cmd = $entityManager->getClassMetadata($className);
-    $connection->beginTransaction();
-    try {
-        $connection->query('SET FOREIGN_KEY_CHECKS=0');
-        $q = $dbPlatform->getTruncateTableSql($cmd->getTableName());
-        $connection->executeUpdate($q);
-        $connection->query('SET FOREIGN_KEY_CHECKS=1');
-        $connection->exec('ALTER TABLE ' . $cmd->getTableName() . ' AUTO_INCREMENT = 1;');
-        $connection->commit();
-    } catch (\Exception $e) {
-        $connection->rollback();
-    }
-}
