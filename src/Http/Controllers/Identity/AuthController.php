633e984bd7db41c1e320cc22923ba5a61f39aaf8
bakgat <karl.vaniseghem@klimtoren.be>
2015-12-15 13:21:44 +0100

    translate login failed message

--- src/Http/Controllers/Identity/AuthController.php
+++ src/Http/Controllers/Identity/AuthController.php
@@ -94,7 +94,7 @@ class AuthController extends Controller
      */
     protected function getFailedLoginMessage()
     {
-        return 'These credentials do not match our records.';
+        return 'De combinatie van gebruikersnaam en paswoord is niet correct.';
     }
 
     /**
